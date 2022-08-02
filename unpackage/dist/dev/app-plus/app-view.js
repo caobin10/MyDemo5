ddb06d67e9c52953468b2c63cd242b9a69ac82f3
caobin10 <1562235042@qq.com>
2022-08-02 21:07:26 +0800

    666

--- unpackage/dist/dev/app-plus/app-view.js
+++ unpackage/dist/dev/app-plus/app-view.js
@@ -267,7 +267,7 @@ var render = function() {
     [
       _c("uni-view", { staticClass: _vm._$g(1, "sc"), attrs: { _i: 1 } }, [
         _vm._v(
-          "学校 本项目已包含uni ui组件，无需import和注册，可直接使用。在代码区键入字母u，即可通过代码助手列出所有可用组件。光标置于组件名称处按F1，即可查看组件文档。"
+          "小可爱 本项目已包含uni ui组件，无需import和注册，可直接使用。在代码区键入字母u，即可通过代码助手列出所有可用组件。光标置于组件名称处按F1，即可查看组件文档。"
         )
       ]),
       _c("v-uni-text", { staticClass: _vm._$g(2, "sc"), attrs: { _i: 2 } }, [
